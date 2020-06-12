7bea4424d2b179538a9eb97633fd5f1d04c2ceee
Steven Bal <steven@maykinmedia.nl>
2020-06-12 16:29:05 +0200

    Added queryparameter for voorkeurstaal

--- src/contactmomenten/api/filters.py
+++ src/contactmomenten/api/filters.py
@@ -1,9 +1,15 @@
 from vng_api_common.filtersets import FilterSet
 
-from contactmomenten.datamodel.models import ObjectContactMoment
+from contactmomenten.datamodel.models import ContactMoment, ObjectContactMoment
 
 
 class ObjectContactMomentFilter(FilterSet):
     class Meta:
         model = ObjectContactMoment
         fields = ("object", "contactmoment")
+
+
+class ContactMomentFilter(FilterSet):
+    class Meta:
+        model = ContactMoment
+        fields = ("voorkeurstaal",)
